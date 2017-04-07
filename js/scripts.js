f3444271293b03ce0a96d7cf9d00c46fc56ba725
peter <pkarella27@yahoo.com>
2017-04-07 10:36:29 -0700

    continue to work on structure of site

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,25 @@
 //B.L.
 
-
-var Pizza = function (name,topping, size) {
-  this.name = name;
+var price =0;
+var Pizza = function (topping) {
   this.topping = topping;
-  this.size = size;
 }
 
-Pizza.prototype.cost = function(top,siz) {
-  var cost=0;
-  var result = if(this.topping>=top && this.topping==="1"){
-    ("#outputOne").text("Cost is $15");
-  } else if (this.toppin<=inputToppings){
-    ("#outputTwo").text("Cost is $10");
-  }
-  return result;
-
+Pizza.prototype.cost = function() {
+   if(this.topping === "Sausage"){
+   price+=5;
+} return price;
 }
 
 //U.I.
 
 $(document).ready(function() {
-$("form#formone").submit(function(event) {
+$("#formone").submit(function(event) {
     event.preventDefault();
- var newPizza= new Pizza(inputToppings,inputSize);
- $("input:checkbox[name=toppings]:checked").each(function(){
-   var inputToppings = $(this).val();
-   var inputSize = $("#size").val();
 
-newPizza.cost();
-});
+ var inputTopping = $("#topping").val();
+var newPizza= new Pizza(inputToppings);
+ newPizza.cost();
 
   });
-
-
 });
