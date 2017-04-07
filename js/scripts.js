291c9b7660e03fcc2e47d41b4eb3e05c4d26dce9
peter <pkarella27@yahoo.com>
2017-04-07 12:03:15 -0700

    continue to refine logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 //B.L.
-
-var price =0;
-var Pizza = function (name,topping, sauce, size) {
-  this.topping = topping;
+function Pizza(name,topping,sauce,crust,size,price){
+this.name= name;
+this.topping = topping;
+this.sauce = sauce;
+this.crust = crust;
+this.size = size;
+this.price = 10;
 }
 
-Pizza.prototype.cost = function() {
-   if(this.topping === "Sausage"){
-   price+=5;
-} return price;
+
+Pizza.prototype.cost = function(){
+  if (this.topping ==="sausage" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
+  return this.price + 2;
+} else if (this.topping ==="mushroom"&& this.sauce ==="meat"&& this.crust === "thick" && this.size === "large"){
+return this.price + 3;
 }
+}
+
 
 //U.I.
 
 $(document).ready(function() {
 $("#formone").submit(function(event) {
     event.preventDefault();
+    var resultName = $("#name").val();
+    var resultTopping = $("#topping").val();
+    var resultSauce = $("#sauce").val();
+    var resultCrust = $("#crust").val();
+    var resultSize = $("#size").val();
+    var newPizza = new Pizza (resultName, resultTopping, resultSauce, resultCrust,resultSize);
 
- var inputTopping = $("#topping").val();
-var newPizza= new Pizza(inputToppings);
- newPizza.cost();
+    $("p#outputOne").text(newPizza.cost());
 
   });
 });
