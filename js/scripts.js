015b5c3effa9cf752238f24c1f711ad571e5094d
peter <pkarella27@yahoo.com>
2017-04-11 20:22:17 -0700

    revising

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ this.topping = topping;
 this.sauce = sauce;
 this.crust = crust;
 this.size = size;
-this.price = 10;
+this.price = 0;
 }
 
 Pizza.prototype.greeting = function(){
@@ -29,23 +29,64 @@ return ("Thankyou for your order "+ this.name +" Enjoy!!");
 
 Pizza.prototype.cost = function(){
 
-  if ((this.topping>=5 && this.sauce ==="meat" && this.crust === "thick" && this.size === "large")||(this.topping>=5 && this.sauce ==="oliveoil" && this.crust === "thick" && this.size === "large")) {
-     this.price += 6;
-     return this.price;
-   } else if (this.topping===4 && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
-      this.price += 5;
-      return this.price;
-    } else if (this.topping===3 && this.sauce ==="marinara" && this.crust === "thick" && this.size === "small")||(this.topping===3 && this.sauce ==="meat" && this.crust === "thick" && this.size === "small"){
-       this.price += 4;
-       return this.price;
-    }else if (this.topping<=2  && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
-   this.price += 3;
-   return this.price;
-   }
-
-
-
+  if(this.sauce=== "marinara"){
+    this.price += 1;
+  }
+  else if(this.sauce=== "meat"){
+    this.price += 2;
+  }
+  else if(this.sauce=== "oliveoil"){
+    this.price += 1;
+  }
+  else if(this.crust=== "thin"){
+    this.price += 1;
+  }
+  else if(this.crust=== "thick"){
+    this.price += 2;
 }
+ else if(this.size=== "small"){
+  this.price += 5;
+}
+ else if(this.size=== "medium"){
+  this.price += 7;
+}
+ else if(this.size=== "large"){
+  this.price += 9;
+}
+
+  if(this.topping=== 6){
+    this.price += 4;
+  }
+  else if(this.topping === 4 ){
+    this.price +=3;
+  }
+  else if(this.topping === 3){
+    this.price +=2;
+  }
+  else if(this.topping<=2){
+    this.price +=1;
+  }
+
+  return this.price;
+}
+
+//   if ((this.topping>=5 && this.sauce ==="meat" && this.crust === "thick" && this.size === "large")||(this.topping>=5 && this.sauce ==="oliveoil" && this.crust === "thick" && this.size === "large")) {
+//      this.price += 6;
+//      return this.price;
+//    } else if (this.topping===4 && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
+//       this.price += 5;
+//       return this.price;
+//     } else if (this.topping===3 && this.sauce ==="marinara" && this.crust === "thick" && this.size === "small")||(this.topping===3 && this.sauce ==="meat" && this.crust === "thick" && this.size === "small"){
+//        this.price += 4;
+//        return this.price;
+//     }else if (this.topping<=2  && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
+//    this.price += 3;
+//    return this.price;
+//    }
+//
+//
+//
+// }
 
 
 
@@ -60,18 +101,19 @@ $(document).ready(function() {
     var resultName = $("#name").val();
     $("input:checkbox[name=topping]:checked").each(function(){
     toppingArray.push($(this).val());
+});
 
-    });
     var resultSauce = $("#sauce").val();
     var resultCrust = $("#crust").val();
     var resultSize = $("#size").val();
-    var finalTopping= toppingArray.length;
+    var finalPizzaCost= toppingArray.length;
 
-var newPizza = new Pizza (resultName,finalTopping,resultSauce, resultCrust,resultSize);
+var newPizza = new Pizza (resultName,finalPizzaCost,resultSauce, resultCrust,resultSize);
 console.log(newPizza);
 
 
-    $("p#outputOne").text("Your Total Will Be " + "$" + newPizza.cost(toppingArray));
+    $("#formone").empty();
+    $("p#outputOne").text("Your Total Will Be " + "$" + newPizza.cost());
     $("p#outputTwo").text(newPizza.greeting());
 
 
