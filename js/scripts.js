74daa495baa12304d3c5b8d859fbae8eb8266333
peter <pkarella27@yahoo.com>
2017-04-12 20:15:05 -0700

    final

--- js/scripts.js
+++ js/scripts.js
@@ -10,51 +10,34 @@ this.price = 0;
 
 Pizza.prototype.greeting = function(){
 return ("Thankyou for your order "+ this.name +" Enjoy!!");
-
 }
 
 
-
-// Pizza.prototype.cost =function(){
-// if ((this.topping ==="garlic" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small")||(this.topping ==="mushroom" && this.sauce ==="meat" && this.crust === "thin" && this.size === "small")||(this.topping === "garlic" && this.sauce ==="oliveoil" && this.crust === "thin" && this.size === "small")||(this.topping ==="sausage"&& this.sauce ==="marinara"&& this.crust === "thin" && this.size === "small")||(this.topping === "mushroom" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small")) {
-//   return this.price + 2;
-// } else if ((this.topping ==="garlic"&& this.sauce ==="oliveoil"&& this.crust === "thick" && this.size === "medium")||(this.topping ==="mushroom"&& this.sauce ==="meat"&& this.crust === "thick" && this.size === "medium")||(this.topping === "sausage" && this.sauce ==="oliveoil" && this.crust === "thin" && this.size === "small")||(this.topping ==="mushroom"&& this.sauce ==="marinara"&& this.crust === "thick" && this.size === "medium")) {
-// return this.price + 3;
-// }else if ((this.topping ==="sausage" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "large")||(this.topping ==="mushroom" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "large")||(this.topping === "sausage" && this.sauce ==="meat" && this.crust === "thick" && this.size === "large")||(this.topping ==="garlic" && this.sauce ==="marinara" && this.crust === "thin" && this.size === "large")) {
-//   return this.price + 4;
-//
-// }
-// }
 
 
 Pizza.prototype.cost = function(){
 
-  if(this.sauce=== "marinara"){
+      if(this.sauce=== "marinara"){
+    this.price += 1;
+  } else if(this.sauce=== "meat"){
+    this.price += 2;
+  } else if(this.sauce=== "oliveoil"){
     this.price += 1;
   }
-  else if(this.sauce=== "meat"){
+    if(this.crust=== "thin"){
+    this.price += 1;
+  } else if(this.crust=== "thick"){
     this.price += 2;
   }
-  else if(this.sauce=== "oliveoil"){
-    this.price += 1;
-  }
-  else if(this.crust=== "thin"){
-    this.price += 1;
-  }
-  else if(this.crust=== "thick"){
-    this.price += 2;
-}
- else if(this.size=== "small"){
+    if(this.size=== "small"){
   this.price += 5;
-}
- else if(this.size=== "medium"){
+  } else if(this.size=== "medium"){
   this.price += 7;
-}
- else if(this.size=== "large"){
+  } else if(this.size=== "large"){
   this.price += 9;
-}
+  }
 
-  if(this.topping=== 6){
+    if(this.topping=== 6){
     this.price += 4;
   }
   else if(this.topping === 4 ){
@@ -70,23 +53,7 @@ Pizza.prototype.cost = function(){
   return this.price;
 }
 
-//   if ((this.topping>=5 && this.sauce ==="meat" && this.crust === "thick" && this.size === "large")||(this.topping>=5 && this.sauce ==="oliveoil" && this.crust === "thick" && this.size === "large")) {
-//      this.price += 6;
-//      return this.price;
-//    } else if (this.topping===4 && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
-//       this.price += 5;
-//       return this.price;
-//     } else if (this.topping===3 && this.sauce ==="marinara" && this.crust === "thick" && this.size === "small")||(this.topping===3 && this.sauce ==="meat" && this.crust === "thick" && this.size === "small"){
-//        this.price += 4;
-//        return this.price;
-//     }else if (this.topping<=2  && this.sauce ==="marinara" && this.crust === "thin" && this.size === "small"){
-//    this.price += 3;
-//    return this.price;
-//    }
-//
-//
-//
-// }
+
 
 
 
@@ -101,31 +68,24 @@ $(document).ready(function() {
     var resultName = $("#name").val();
     $("input:checkbox[name=topping]:checked").each(function(){
     toppingArray.push($(this).val());
-});
+    });
 
     var resultSauce = $("#sauce").val();
     var resultCrust = $("#crust").val();
     var resultSize = $("#size").val();
     var finalPizzaCost= toppingArray.length;
 
-var newPizza = new Pizza (resultName,finalPizzaCost,resultSauce, resultCrust,resultSize);
-console.log(newPizza);
+    var newPizza = new Pizza (resultName,finalPizzaCost,resultSauce, resultCrust,resultSize);
 
 
-    $("#formone").empty();
+
+    $("#output").show();
+    $("#formone").hide();
     $("p#outputOne").text("Your Total Will Be " + "$" + newPizza.cost());
     $("p#outputTwo").text(newPizza.greeting());
-
-
-
-    $("#buttontwo").click(function(){
-        $("p#outputOne").hide();
+    $("#buttontwo").click(function(event){
+      $("#formone").show();
+      $("#output").hide();
     });
-    // $("#name").val('');
-    //  $("#topping").val('');
-    //  $("#sauce").val('');
-    //  $("#crust").val('');
-    // $("#size").val('');
-
   });
 });
---
+++ readme.md
@@ -0,0 +1,36 @@
+# _Pizza_
+
+#### _Pizza Order Form, April/12/2017_
+
+#### By _**Peter Karellas**_
+
+#### Contributers
+_Peter Karellas_
+
+## Description
+
+_A Basic Pizza Order Delivery Form. You can choose your toppings and size and receive
+the total amount_
+
+## Setup/Installation Requirements
+
+*_Calculate cost for small pizza based on wether its	Small,Medium,or Large Pizza._
+*_Calculate cost for pizza based on thickness of crust._
+*_Calculate cost for Pizza  in regards to the amount of toppings_
+*_Calculate cost for Pizza with a certain sauce_
+
+## Known Bugs
+
+_N/A_
+
+## Support and contact details
+
+_Peter Karellas pkarella27@yahoo.com_
+
+## Technologies Used
+
+_HTML,CSS,Javascript,Bootstrap_
+
+### License
+
+_mit_
